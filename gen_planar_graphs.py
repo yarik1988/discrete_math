24bf8d8e97c631c2916250188f8bfdaf2c975159
yarik1988 <yaroslav.linder@gmail.com>
2026-09-04 16:27:59 +0300

    update

--- gen_planar_graphs.py
+++ gen_planar_graphs.py
@@ -2,11 +2,11 @@ import os
 from sage.all import graphs
 from tqdm import tqdm
 # Use Sage's built-in generator
-os.makedirs("output_gen", exist_ok=True)
+os.makedirs("out_misc_graphs", exist_ok=True)
 counter=0
 iterator = graphs.planar_graphs(12,minimum_degree=5)
 for i,G in tqdm(enumerate(iterator)):
         if G.is_connected() and G.is_regular(5):
                 counter+=1
-                G.plot(layout='planar').save(os.path.join("output_gen",f"my_graph_{i}.png"))
+                G.plot(layout='planar').save(os.path.join("out_misc_graphs", f"my_graph_{i}.png"))
 print(counter)
